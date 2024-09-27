e3b328f4cca0396f6167bf2b76080d9e4f9fb226
mayola <mayola@marketpsychdata.com>
2024-09-26 23:24:48 -0300

    added requirements, gitignore, products table && removed images that had to much weight

--- App/models.py
+++ App/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from ckeditor import fields
 # Create your models here.
-class Personajes(models.Model):
-    Codigo=models.AutoField(primary_key=True)
-    Nombre=models.TextField(max_length=30)
-    Casa=models.TextField(max_length=15)
-    Hechizo=models.TextField(max_length=20)
-    Imagen=models.ImageField(upload_to="Personajes",null=True)
+class Productos(models.Model):
+    id_producto = models.IntegerField(primary_key=True, null=False)
+    nom_producto = models.TextField(max_length=50, null=False)
+    desc_producto = fields.RichTextField(null=True)
+    precio_producto = models.IntegerField(null=False)
+    precioreb_producto = models.IntegerField(null=True)
+    stock_producto = models.IntegerField(null=False)
+    def __int__ (self):
+        return self.id_producto
 
-    def __int__(self):
-        self.Codigo
